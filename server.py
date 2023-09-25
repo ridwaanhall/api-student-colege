cdba0303a2359716e8ef158c833edeb6ed5837fd
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-25 13:13:19 +0000

    route

--- server.py
+++ server.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from Controller.StudentController import get_student_data
 
 app = Flask(__name__)
 
 
-@app.route("/search_student/<message_search>")
-def search_student():
-  return ""
+@app.route("/")
+def home():
+    return "anjaaayyyyyy"
+
+
+@app.route("/<message_search>")
+def search_student(message_search):
+    student_data = get_student_data(message_search)
+
+    if student_data is not None:
+        return jsonify(student_data)
+    else:
+        return "Error fetching data from API", 500
